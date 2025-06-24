f55c18c6663ee854bba090754b0bda95a81e870b
ahakimov <a.khakimov.22@gmail.com>
2025-06-24 17:53:53 +0300

    add mechanics code

--- src/main/java/com/example/carrepairshop/model/Mechanic.java
+++ src/main/java/com/example/carrepairshop/model/Mechanic.java
@@ -1,4 +1,14 @@
 package com.example.carrepairshop.model;
 
+import lombok.Data;
+
+@Data
 public class Mechanic {
+    String id;
+    String name;
+    String phone;
+    String email;
+    String password;
+    String specialty;
+    Long experience;
 }
